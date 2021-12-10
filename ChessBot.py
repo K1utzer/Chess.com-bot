72afa73ef8dc64b4770246d0188224e50a89a402
Lutz Wolf <wolf-98@gmx.de>
2021-12-10 16:21:27 +0100

    Make it work with only image recognition

---
+++ ChessBot.py
@@ -0,0 +1,32 @@
+import configparser
+import chess
+
+import time
+
+import cv2 as cv
+import numpy as np
+
+import re
+
+import os
+
+import random
+
+import pyautogui
+
+import keyboard
+import win32api
+import win32con
+
+from stockfishpy.stockfishpy import *
+
+
+def init():
+    pass
+
+def pictureDetection(imgIn, imgToSearch):
+    pass
+class Feld:
+    
+    def __init__(self):
+        board = chess.Board()
