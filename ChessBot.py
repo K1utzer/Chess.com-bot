4799834e57a3291140b767d731476a44b10d7938
K1utzer <74728972+K1utzer@users.noreply.github.com>
2022-04-07 19:12:05 +0200

    Python version check and warning

--- ChessBot.py
+++ ChessBot.py
@@ -7,7 +7,7 @@ import cv2 as cv
 import numpy as np
 
 import re
-
+import sys
 import os
 
 import random
@@ -291,5 +291,12 @@ class BoardControl:
         #print(fields_Cords)
         return fields_Cords
 
+
+if sys.version_info[0] < 3:
+    raise Exception("Must be using Python version 3. Developed with Python 3.8.8")
+if sys.version_info[1] != 8:
+    print("WARNING: Developed with Python version 3.8.8. May not work properly if used with another version")
+elif sys.version_info[2] != 8:
+    print("WARNING: Developed with Python version 3.8.8. May not work properly if used with another version")
 
 manager = Manager()
